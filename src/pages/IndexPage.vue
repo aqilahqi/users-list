32bd7211da1411ad1dafe7eab7d853c6f2f771dc
Aqilah <aqilahfatin01@gmail.com>
2023-03-11 04:17:44 +0800

    add search

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,13 +2,14 @@
   <q-page>
     <div>
       <ProfileHeader />
-      <div class="q-pa-md">index</div>
+      <div class="q-pa-md"><TableList /></div>
     </div>
   </q-page>
 </template>
 
 <script setup>
 import ProfileHeader from "src/components/ProfileHeader.vue";
+import TableList from "src/components/TableList.vue";
 </script>
 
 <style lang="scss" scoped></style>
